eebc53115deaa30cbf7e4b53de41f8d1c863c784
kaiwen0425 <kaiweno425@gmail.com>
2019-09-01 11:07:03 +0800

    for base_api script

--- creact_base_api.sh
+++ creact_base_api.sh
@@ -1,4 +1,12 @@
-git clone git@github.com:Binaryify/NeteaseCloudMusicApi.git ../BASE_music_api
+base_files=`../BASE_music_api/`
+if [ -e ${base_files} ]
+then
+  echo "the base_api files is ok , start the server......"
+else
+  git clone git@github.com:Binaryify/NeteaseCloudMusicApi.git ../BASE_music_api
+fi
+
 cd ../BASE_music_api/
+git pull
 npm install
 node app.js
